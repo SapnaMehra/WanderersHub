6e6897a3ea002e4bb210f13cedebf8a8e9c58585
Sapna Mehra <sapnamehra0111@gmail.com>
2026-09-13 12:52:49 +0530

    Fix responsive mobile layout

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ const Review = require("./models/review");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
-    req.flash("error", "You must be logged in to create a listing!");
+    req.flash("error", "You must be logged in first!");
     return res.redirect("/login");
   }
   next();
